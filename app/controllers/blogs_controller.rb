fe7e1c9bda304f049b122b1e3892698b2f8983e9
Yashwardhan Singh Chandel <ytheading10@gmail.com>
2023-05-28 00:26:20 +0530

    Create action added

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,22 @@
 class BlogsController < ApplicationController
     def index
-    @blogs=Blog.all
+        @blogs=Blog.all
     end
+
+    def show
+        @show_blog = Blog.find(params[:id])
+    end
+
+
+    def create
+        @new_blog = Blog.new(blog_params)
+        if @new_blog.save
+            redirect_to root_path
+        end
+    end
+
+    private
+        def blog_params
+            params.require(:blog).permit(:title, :body)
+        end
 end
